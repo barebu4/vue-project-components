02a288f044c0bb88beba2cbabc7b185bb1fbb228
barebu4 <ivanmahnyuk591@gmail.com>
2025-06-17 16:53:03 +0300

    первая версия проекта

---
+++ app.vue
@@ -0,0 +1,23 @@
+<script setup>
+  import { ref } from 'vue';
+  import ChildItem from './components/ChildItem.vue';
+
+  let userNames = ref(["Ivan"])
+
+  function addNames(){
+    userNames.value.push("")
+    // console.log(userNames.value)
+  }
+</script>
+
+<template>
+  <h1>Users list</h1>
+  <h3>Add User</h3>
+  <button @click="addNames">Add user</button>
+  <div v-for="(name, index) in userNames" :key="index">
+    <ChildItem v-model="userNames[index]"/>
+  </div>
+  <ul>
+    <li v-for="(name, index) in userNames" :key="index">{{ name }} </li>
+  </ul>
+</template>
---
+++ components/ChildItem.vue
@@ -0,0 +1,17 @@
+<script setup>
+    import { computed } from 'vue'
+
+    const props = defineProps({
+        modelValue: String
+    })
+    const emit = defineEmits(["update:modelValue"])
+
+    const name = computed({
+        get: () => props.modelValue,
+        set: (value) => emit("update:modelValue", value)
+    })
+</script>
+
+<template>
+    <input v-model="name" placeholder="Write user name">
+</template>
